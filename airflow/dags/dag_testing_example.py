4bd1e32fd21a32103f7e398f1d06c6f44e9827e7
audrio <zerotwo8802@gmail.com>
2026-01-10 20:39:37 +0700

    add dags file

---
+++ airflow/dags/dag_testing_example.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+def transform_data(**context):
+    data = context['params']['data']
+    return [d.upper() for d in data]
+
+with DAG("dag_testing_example", start_date=datetime(2024,1,1), schedule=None) as dag:
+    
+    start_task = EmptyOperator(task_id="start_task")
+    end_task   = EmptyOperator(task_id="end_task")
+
+    transform = PythonOperator(
+        task_id="transform_task",
+        python_callable=transform_data,
+        params={'data': ['apple', 'banana']}
+    )
+
+    start_task >> transform >> end_task
