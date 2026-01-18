ed785860e3532aeff041dc89b3c857baf7e9a0ff
audrio <zerotwo8802@gmail.com>
2026-01-19 02:21:19 +0700

    refactor: add comment current context in task instance

--- airflow/dags/xcom_with_return_and_current_context.py
+++ airflow/dags/xcom_with_return_and_current_context.py
@@ -24,3 +24,13 @@ def xcom_with_return_and_current_context():
     start_task >> sender() >> receiver() >> end_task
 
 xcom_with_return_and_current_context()
+
+# current context yaitu metadata dari task instance
+# Beberapa key yang biasanya ada di kwargs:
+# - ti → TaskInstance object, dipakai untuk xcom_push dan xcom_pull.
+# - dag → objek DAG yang sedang berjalan.
+# - task → objek task yang sedang dieksekusi.
+# - ds → execution date dalam format string (YYYY-MM-DD).
+# - ts → timestamp eksekusi.
+# - dag_run → informasi tentang DAG run saat ini.
+
